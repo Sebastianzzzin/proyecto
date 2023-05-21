17801524c691d646a6a6f1641f792b26c8d7a89d
Sebastian Alfonso Huacasi <aalfonso@unsa.edu.pe>
2023-05-21 14:13:27 -0500

    Laboratorio 01

---
+++ lab01/Bienvenida.java
@@ -0,0 +1,5 @@
+public class Bienvenida{
+	public static void main(String[]args){
+		System.out.println("Bienvenido al Laboratorio de PWEB 2");
+	}
+}
---
+++ lab01/ImprimeNombre.java
@@ -0,0 +1,5 @@
+public class ImprimirNombre{
+	public static void main(String[]args){
+		System.out.println("Alejandro Sebastian Alfonso Huacasi");
+	}
+}
