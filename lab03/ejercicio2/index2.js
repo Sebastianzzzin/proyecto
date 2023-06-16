47da225807272ba1dbaa40e8ac0e0ed4fde9171f
Sebastianzzzin <120695582+Sebastianzzzin@users.noreply.github.com>
2023-06-16 15:18:08 -0500

    Update index2.js

--- lab03/ejercicio2/index2.js
+++ lab03/ejercicio2/index2.js
@@ -8,5 +8,5 @@ app.listen(3000, () => {
 });
 
 app.get('/', (request, response) => {
-    response.sendFile(path.resolve(__dirname, 'index.html'));
+    response.sendFile(path.resolve(__dirname, 'index2.html'));
 });
